69d7d26b54c72aaaf7a808e71d38ae27912d42b9
destoswa <84578314+destoswa@users.noreply.github.com>
2021-11-24 23:03:05 +0100

    PCB and PAH of interest

--- PCB_PAH.m
+++ PCB_PAH.m
@@ -4,7 +4,10 @@ clc;
 set(0,'DefaultFigureWindowStyle','docked');
 load('data_PCB_PAH.mat');
 
-%plotting the graphs for all PCB
+% -------------------------------------------------------------------------
+% Plotting the graphs for all PCB
+% -------------------------------------------------------------------------
+
 fig_PCB_all = figure(1)
 PCB_concentrations = zeros(12,13);
 %for each PCB:
@@ -29,7 +32,10 @@ for i=1:12
 end
 suptitle('All PCBs detected in the sites of interest');
 % saveas(fig_PCB_all,'./figures/PCB_all.png');
-%Calculation of total PCBs:
+
+% -------------------------------------------------------------------------
+% Computing and plotting of total PCBs:
+% -------------------------------------------------------------------------
 PCB_totaux = zeros(13,1);
 PCB_totaux = sum(PCB_concentrations);
 
@@ -46,8 +52,9 @@ grid minor
 
 
 
-
-%plotting the graph for the 3 PCB of interest
+% -------------------------------------------------------------------------
+% Plotting the graph for the 3 PCB of interest
+% -------------------------------------------------------------------------
 fig_PCB_ofInterest = figure(3);
 for i=7:9
     pl = polyfit(PCB_calibrate(i,1:2:end),PCB_calibrate(i,2:2:end),1);
@@ -61,10 +68,12 @@ for i=7:9
         'color','black','Markersize',4,'MarkerFaceColor','black');
     %data values
     plot((PCB_datas(i,:))./pl(1),PCB_datas(i,:),'*','color','blue'...
-        ,'markersize',8);    
+        ,'markersize',8);
+    plot(PCB_datas(i,1)/pl(1),PCB_datas(i,1),'o','markersize',6,...
+        'MarkerFaceColor',[.0 1 .0],'MarkerEdgeColor',[.0 .3 .0]);
     hold off
     title(PCB_elements(i));
-    legend('linear regression','calibrating values','Measured values','location','northwest');
+    legend('linear regression','calibrating values','Measured values',"1M's value",'location','northwest');
     xlabel('Concentration [ng/ml]');
     ylabel('Surface');
     xmax = max([PCB_datas(i,:)./pl(1) PCB_calibrate(i,1:2:end)]);
@@ -84,30 +93,33 @@ for i=7:9
     plot(PCB_calibrate(i,1:2:end),PCB_calibrate(i,2:2:end),'o',...
         'color','black','Markersize',4,'MarkerFaceColor','black');
     %data values
-    plot((PCB_datas(i,:))./pl(1),PCB_datas(i,:),'*','color','blue'...
-        ,'markersize',8);
+    plot(PCB_datas(i,:)./pl(1),PCB_datas(i,:),'*','color','blue'...
+        ,'markersize',6);
     %enlighting our result:
     x_1M = PCB_datas(i,1)/pl(1);
     y_1M = PCB_datas(i,1);
     color = [.3 .3 .3];
-    plot([0 x_1M],[y_1M y_1M],'--','color',color,'LineWidth',1);
-    plot([x_1M x_1M],[0 y_1M],'--','color',color,'LineWidth',1);
-    text(x_1M+0.02,y_1M/2,[num2str(round(x_1M,3)) ' ng/ml']...
+    plot([0 x_1M],[y_1M y_1M],'--','color',color,'LineWidth',1,'HandleVisibility','off');
+    plot([x_1M x_1M],[0 y_1M],'--','color',color,'LineWidth',1,'HandleVisibility','off');
+    plot(PCB_datas(i,1)/pl(1),PCB_datas(i,1),'o','markersize',6,...
+        'MarkerFaceColor',[.0 1 .0],'MarkerEdgeColor',[.0 .3 .0]);
+    text(x_1M*1.1,y_1M/2,[num2str(round(x_1M,3)) ' ng/ml']...
         ,'color',color,'Fontsize',12,'Fontweight','bold');
     hold off
     title('zoom');
-    legend('linear regression','calibrating values','Measured values','location','northwest');
+    legend('linear regression','calibrating values','Measured values',...
+        "1M's value",'location','northwest');
     xlabel('Concentration [ng/ml]');
     ylabel('Surface');
-%     xmax = max([PCB_datas(i,:)./pl(1) PCB_calibrate(i,1:2:end)]);
     xlim([0 3*x_1M]);
-%     ymax = max([PCB_datas(i,:) PCB_calibrate(i,2:2:end)]);
     ylim([0 2*y_1M]);
 end
 suptitle('PCB of interest for group 1M');
 % saveas(fig_PCB_ofInterest,'./figures/PCB_ofInterest.png');
 
-%plotting the graph for all PAH:
+% -------------------------------------------------------------------------
+% Plotting the graph for all PAH:
+% -------------------------------------------------------------------------
 PAH_concentrations = zeros(16,13);
 fig_PAH_all = figure(4);
 for i=4:16
@@ -132,7 +144,9 @@ end
 suptitle('All PAHs detected in the sites of interest');
 % saveas(fig_PAH_all,'./figures/PAH_all.png');
 
-%Calculation of total PAHs:
+% -------------------------------------------------------------------------
+% Computation and plotting of total PAHs:
+% -------------------------------------------------------------------------
 PAH_totaux = zeros(13,1);
 PAH_totaux = sum(PAH_concentrations);
 
@@ -147,24 +161,30 @@ grid on
 grid minor
 % saveas(fig_PAH_Tot,'figures/PAH_Totaux.png');
 
-%plotting the graph for the 6 PAH of interest
+% -------------------------------------------------------------------------
+% Plotting the graph for the 6 PAH of interest
+% -------------------------------------------------------------------------
 fig_PAH_interest = figure(6);
 for i=4:10
     if i ~= 9
         pl = polyfit(PAH_calibrate(i,1:2:end),PAH_calibrate(i,2:2:end),1);
-        x = linspace(PAH_calibrate(i,1),PAH_calibrate(i,11),200);
+        x = linspace(0,PAH_calibrate(i,11),200);
         if i == 10
-            subplot(2,3,6);
+            subplot(6,2,11);
         else
-            subplot(2,3,i-3);
+            subplot(6,2,(i-3)*2-1);
         end
-        plot(PAH_calibrate(i,1:2:end),PAH_calibrate(i,2:2:end),'d','color','blue');
+        plot(x,pl(1)*x,'-','color','red','LineWidth',1.2);
         hold on
-        plot(x,pl(1)*x,'-','color','blue');
-        plot((PAH_datas(i,:))./pl(1),PAH_datas(i,:),'x','color','red');
+        plot(PAH_calibrate(i,1:2:end),PAH_calibrate(i,2:2:end),'o',...
+        'color','black','Markersize',4,'MarkerFaceColor','black');
+        plot((PAH_datas(i,:))./pl(1),PAH_datas(i,:),'*','color','blue'...
+        ,'markersize',8);
+        plot(PAH_datas(i,1)/pl(1),PAH_datas(i,1),'o','markersize',6,...
+        'MarkerFaceColor',[.0 1 .0],'MarkerEdgeColor',[.0 .3 .0]);
         hold off
         title(PAH_elements(i));
-        legend('calibrating values','linear regression','Measured values','location','northwest');
+        legend('calibrating values','linear regression','Measured values',"1M's value",'location','northwest');
         xlabel('Concentration [ng/ml]');
         xmax = max([PAH_datas(i,:)./pl(1) PAH_calibrate(i,1:2:end)]);
         xlim([0 xmax+100]);
@@ -173,5 +193,40 @@ for i=4:10
         ylabel('Surface');
     end
 end
+%zoom on our data
+for i=4:10
+    if i ~= 9
+        pl = polyfit(PAH_calibrate(i,1:2:end),PAH_calibrate(i,2:2:end),1);
+        x = linspace(0,PAH_calibrate(i,11),200);
+        if i == 10
+            subplot(6,2,12);
+        else
+            subplot(6,2,(i-3)*2);
+        end
+        plot(x,pl(1)*x,'-','color','red','LineWidth',1.2);
+        hold on
+        plot(PAH_calibrate(i,1:2:end),PAH_calibrate(i,2:2:end),'o',...
+        'color','black','Markersize',4,'MarkerFaceColor','black');
+        plot((PAH_datas(i,:))./pl(1),PAH_datas(i,:),'*','color','blue'...
+        ,'markersize',8);
+        %enlighting our result:
+        x_1M = PAH_datas(i,1)/pl(1);
+        y_1M = PAH_datas(i,1);
+        color = [.3 .3 .3];
+        plot([0 x_1M],[y_1M y_1M],'--','color',color,'LineWidth',1,'HandleVisibility','off');
+        plot([x_1M x_1M],[0 y_1M],'--','color',color,'LineWidth',1,'HandleVisibility','off');
+        plot(PAH_datas(i,1)/pl(1),PAH_datas(i,1),'o','markersize',6,...
+        'MarkerFaceColor',[.0 1 .0],'MarkerEdgeColor',[.0 .3 .0]);
+        text(x_1M*1.1,y_1M/2,[num2str(round(x_1M,3)) ' ng/ml']...
+            ,'color',color,'Fontsize',12,'Fontweight','bold');
+        hold off
+        title('zoom');
+        legend('calibrating values','linear regression','Measured values',"1M's value",'location','northwest');
+        xlabel('Concentration [ng/ml]');
+        xlim([0 3.5*x_1M]);
+        ylim([0 2.5*y_1M]);
+        ylabel('Surface');
+    end
+end
 suptitle('PAH of interest for group 1M');
-% saveas(fig_PAH_interest,'./figures/PAH_ofInterest.png');
+% % % % % % % saveas(fig_PAH_interest,'./figures/PAH_ofInterest.png');ne pas lancer cette ligne de code!!!
